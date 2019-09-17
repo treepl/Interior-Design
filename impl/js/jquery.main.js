c4ceea869ce5cbb5e8fc00a543d06f2e4852a56a
elena.l <37115427+codeproduction@users.noreply.github.com>
2019-09-17 17:51:30 +0300

    impl backup

--- impl/js/jquery.main.js
+++ impl/js/jquery.main.js
@@ -5,6 +5,8 @@ jQuery(function () {
     initProjectsCarousel();
     initComingSoonCountDown();
     initHeaderHeight();
+    initAjaxLoadMoreFunction();
+    
 });
 // $('.dropdown-toggle').dropdown();
 
@@ -159,7 +161,7 @@ function initCustomTextCarousel() {
                 touchEnabled: false,
                 minSlides: 1,
                 maxSlides: 2,
-                moveSlides: 1,
+                moveSlides: 2,
                 slideWidth: 570,
                 shrinkItems: true,
                 slideMargin : 30,
@@ -180,6 +182,86 @@ function initCustomTextCarousel() {
 
         }
     }
+}
+
+function initAjaxLoadMoreFunction()
+{
+	var condition = $('.ajaxListHolder').size()
+	    // && false
+	;init(condition);
+
+	function init(condition)
+	{
+	    if(condition || condition == null)
+	    {
+			var $container = $('.ajaxListHolder');
+
+			if($container.find('.next').length)
+			{
+				$('.loadMoreBtn').attr('href', $container.find('.next a').attr('href'));
+				$container.find('.pagination').remove();
+			}
+			else
+			{
+				$('.loadMoreBtn').hide();
+			}
+
+			$container.isotope(
+				{
+					itemSelector: '.load-item',
+					animationEngine: 'best-available',
+					transitionDuration : '0.6s'
+				});
+
+			$('.filterBTNHolder a').on( 'click', function()
+			{
+				var _this = $(this);
+				var filterValue = _this.attr('data-filter');
+
+				$('.filterBTNHolder li').removeClass('active');
+				_this.closest('li').addClass('active');
+
+				$container.isotope({ filter: filterValue });
+
+				return false;
+			});
+
+			$('.loadMoreBtn').click(function()
+			{
+				var _this = $(this);
+				var _href = _this.attr('href');
+
+				$.ajax({
+					url: _href,
+					success: function(data){
+						var requiredObject =  $('div.ajaxListHolder', data);
+						var _items = requiredObject.find('.load-item');
+						var _nextPageLink = requiredObject.find('.next');
+
+						if(_nextPageLink.length)
+						{
+							$('.loadMoreBtn').attr('href', _nextPageLink.find('a').attr('href'));
+						}
+						else
+						{
+							$('.loadMoreBtn').hide();
+						}
+
+						$container.append( _items ).isotope( 'appended', _items ).isotope('layout');
+
+						setTimeout(function()
+						{
+							$('.filterBTNHolder .active a').trigger('click');
+						},500);
+					}
+				});
+
+				// $container.isotope();
+
+				return false;
+			});
+	    }
+	}
 }
 
 
