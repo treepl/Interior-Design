46be100dea5b7e32d6efc60f9b82ea0035a36a85
elena.l <37115427+codeproduction@users.noreply.github.com>
2019-09-23 18:15:50 +0300

    impl beckup

--- impl/js/jquery.main.js
+++ impl/js/jquery.main.js
@@ -6,6 +6,9 @@ jQuery(function () {
     initComingSoonCountDown();
     initHeaderHeight();
     initAjaxLoadMoreFunction();
+    initCounterFunction();
+    initTabNav();
+    initTabNavDrop();
     
 });
 // $('.dropdown-toggle').dropdown();
@@ -129,7 +132,8 @@ function initCustomInfoCarousel() {
                 slideWidth: 270,
                 shrinkItems: true,
                 slideMargin: 30,
-                infiniteLoop: true
+                startSlide: 0,
+                infiniteLoop: false
             });
   
             $('.teamSliderHolder .btn-next').click(function() {
@@ -307,6 +311,108 @@ function initAjaxLoadMoreFunction()
 	}
 }
 
+function initCounterFunction() {
+    var condition = $('.countersHolder').length
+        // && false
+    ;init(condition);
+
+    function init(condition) {
+
+        if (condition || condition == null) {
+            Number.prototype.formatMoney = function (c, d, t) {
+                var n = this,
+                    c = isNaN(c = Math.abs(c)) ? 2 : c,
+                    d = d == undefined ? "." : d,
+                    t = t == undefined ? "," : t,
+                    s = n < 0 ? "-" : "",
+                    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+                    j = (j = i.length) > 3 ? j % 3 : 0;
+                return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+            };
+            $('.countersHolder').viewportChecker({
+                offset: '20%',
+                callbackFunction: function (elem, action) {
+                    $('.countersHolder .box').each(function (index, elm) {
+                        var $container = $(this),
+                            $number = $container.find('.value'),
+                            initial = ($container.data('initial') || '0') + '',
+                            target = ($container.data('target') || '10') + '',
+                            prefix = $container.data('prefix') || '',
+                            suffix = $container.data('suffix') || '',
+                            commaNumber;
+                        // 0 for integers, 1+ for floats (number of digits after the decimal)
+                        precision = 0,
+                            usingComma = false;
+                        if (target.indexOf('.') != -1) {
+                            precision = target.length - 1 - target.indexOf('.');
+                        } else if (target.indexOf(',') != -1) {
+                            precision = target.length - 1 - target.indexOf(',');
+                            usingComma = true;
+                            target = target.replace(/\,/gim, '');
+                        }
+                        initial = window[precision ? 'parseFloat' : 'parseInt'](initial, 10);
+                        target = window[precision ? 'parseFloat' : 'parseInt'](target, 10);
+
+                        if (/bot|googlebot|crawler|spider|robot|crawling/i.test(navigator.userAgent)) {
+                            if (usingComma) {
+                                $number.html(prefix + target.toFixed(precision).replace('\.', ',') + suffix);
+                            } else {
+                                $number.html(prefix + target.toFixed(precision) + suffix);
+                            }
+
+                            return;
+                        }
+
+                        if (usingComma) {
+                            commaNumber = +initial.toFixed(precision);
+                            $number.html(prefix + commaNumber.formatMoney(0, '', ',') + suffix);
+                        } else {
+                            $number.html(prefix + initial.toFixed(precision) + suffix);
+                        }
+
+                        var current = initial,
+                            step = 25,
+                            stepValue = (target - initial) / 25,
+                            interval = setInterval(function (usingComma) {
+                                current += stepValue;
+                                step--;
+                                if (usingComma) {
+                                    commaNumber = +current.toFixed(precision);
+                                    $number.html(prefix + commaNumber.formatMoney(0, '', ',') + suffix);
+                                } else {
+                                    $number.html(prefix + current.toFixed(precision) + suffix);
+                                }
+                                if (step <= 0) {
+                                    if (usingComma) {
+                                        commaNumber = +target.toFixed(precision);
+                                        $number.html(prefix + commaNumber.formatMoney(0, '', ',') + suffix);
+                                    } else {
+                                        $number.html(prefix + target.toFixed(precision) + suffix);
+                                    }
+                                    window.clearInterval(interval);
+                                }
+                            }, 40, usingComma);
+                    });
+                }
+            });
+        }
+    }
+}
+
+// &quot;tab&quot; key handling
+function initTabNav() {
+	jQuery('#navbarNavDropdown').tabNav({
+		hoverClass: 'show',
+		items: 'li.dropdown'
+	});
+}
+function initTabNavDrop() {
+	jQuery('#navbarNavDropdown li.dropdown').tabNav({
+		hoverClass: 'show',
+		items: '.dropdown-menu'
+	});
+}
+
 
 /*Coming Soon*/
 function initComingSoonCountDown() {
@@ -326,3 +432,61 @@ function initComingSoonCountDown() {
     }
 }
 /*End Coming Soon*/
+
+/*
+ * Accessible TAB navigation
+ */
+;(function($){
+	var isWindowsPhone = /Windows Phone/.test(navigator.userAgent);
+	var isTouchDevice = ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch;
+
+	$.fn.tabNav = function(opt) {
+		var options = $.extend({
+			hoverClass: 'hover',
+			items: 'li',
+			opener: '>a',
+			delay: 10
+		},opt);
+
+		if(isWindowsPhone || isTouchDevice) {
+			return this;
+		}
+
+		return this.each(function() {
+			var nav = $(this), items = nav.find(options.items);
+
+			items.each(function(index, navItem) {
+				var item = $(this), navActive, touchNavActive;
+				var link = item.find(options.opener), timer;
+
+				link.bind('focus', function() {
+					navActive = nav.hasClass('js-nav-active');
+					touchNavActive = window.TouchNav && TouchNav.isActiveOn(navItem);
+					if(!navActive || touchNavActive) {
+						initSimpleNav();
+					}
+					item.trigger(navActive && touchNavActive ? 'itemhover' : 'mouseenter');
+				}).bind('blur', function() {
+					item.trigger(navActive && touchNavActive ? 'itemleave' : 'mouseleave');
+				});
+
+				var initSimpleNav = function() {
+					if(!initSimpleNav.done) {
+						initSimpleNav.done = true;
+						item.hover(function() {
+							clearTimeout(timer);
+							timer = setTimeout(function() {
+								item.addClass(options.hoverClass);
+							}, options.delay);
+						}, function() {
+							clearTimeout(timer);
+							timer = setTimeout(function() {
+								item.removeClass(options.hoverClass);
+							}, options.delay);
+						});
+					}
+				};
+			});
+		});
+	};
+}(jQuery));
